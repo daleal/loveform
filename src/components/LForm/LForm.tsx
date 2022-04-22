776735e45a1a206eb3e7a4750f1972256b85e7d6
Daniel Leal <dlleal@uc.cl>
2022-04-21 22:04:58 -0400

    Dispatch the default event if a  event isn't defined for the form

--- src/components/LForm/LForm.tsx
+++ src/components/LForm/LForm.tsx
@@ -1,20 +1,33 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useRender } from '@/utils/render';
 import { createForm } from '@/composables/form';
 
+// Types
+import type { PropType } from 'vue';
+
 export const LForm = defineComponent({
   name: 'LForm',
+  props: {
+    // onSubmit is declared as a hack to be able to detect
+    // when the 'submit' listener is defined
+    // eslint-disable-next-line vue/require-default-prop
+    onSubmit: Function as PropType<(event: Event) => void>,
+  },
   emits: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     submit: (event: Event) => true,
   },
-  setup(_, { emit, slots }) {
+  setup(props, { emit, slots }) {
     const form = createForm();
 
     const onSubmit = (event: Event) => {
       event.preventDefault();
       if (form.valid.value) {
-        emit('submit', event);
+        if (props.onSubmit) {
+          emit('submit', event);
+        } else {
+          ((event as SubmitEvent).target as HTMLFormElement).submit();
+        }
       }
     };
 
