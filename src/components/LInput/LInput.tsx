700285908da04ab94cd5426c5779549935674b48
Daniel Leal <dlleal@uc.cl>
2022-04-26 22:44:23 -0400

    Minor changes to the input defaults

--- src/components/LInput/LInput.tsx
+++ src/components/LInput/LInput.tsx
@@ -8,7 +8,7 @@ export const LInput = defineComponent({
   props: {
     modelValue: {
       type: String,
-      default: '',
+      default: () => '',
     },
     ...makeValidationProps<string>(),
   },
